762f3503d0f66a7938b37548507b144992ef121c
Wojtek Zalewski <wbzwojciechzalewski@gmail.com>
2020-03-17 13:24:36 +0100

    zdeklarowanie formularza przelewu  w komponencie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TransferFormComponent } from './transfer-form/transfer-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransferFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/transfer-form/transfer-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-transfer-form',
+  templateUrl: './transfer-form.component.html',
+  styleUrls: ['./transfer-form.component.css']
+})
+export class TransferFormComponent implements OnInit {
+  transferForm: FormGroup;
+  show: boolean = false;
+  submitted: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.transferForm = new FormGroup({
+      accountNumber: new FormControl('', { 
+        validators: [ 
+          Validators.required, 
+          Validators.minLength(2)
+        ]
+      }),
+      ricipientsName: new FormControl('', { 
+        validators: [
+          Validators.required, 
+          Validators.minLength(2)
+        ]
+      }),
+      ricipientsAddress: new FormControl('', {
+        validators: [
+          Validators.required, 
+          Validators.minLength(5)
+        ]
+      }),
+      transferAmount: new FormControl('', { 
+        validators: [
+          Validators.required,
+          Validators.min(1)
+        ]
+      }),
+      transferTitle: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(2)
+        ]
+      }),
+      transferSender: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(2)
+        ]
+      })
+    });
+  }
+}
