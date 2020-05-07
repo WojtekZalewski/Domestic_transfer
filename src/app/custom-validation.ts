2a37fc9909edf3bb2a0d48c9cd064754dde8afe9
Wojtek Zalewski <wbzwojciechzalewski@gmail.com>
2020-05-07 16:12:37 +0200

    usunięcie pustych linii, format document, usunięcie zbędnego komentarza

--- src/app/custom-validation.ts
+++ src/app/custom-validation.ts
@@ -1,10 +1,9 @@
 import { FormControl, ValidationErrors, ValidatorFn } from "@angular/forms";
-
 export class CustomValidation {
-    static onlyDigits(formControl: FormControl): ValidationErrors {
-        return /[0-9]/.test(formControl.value) ? null : { onlyDigits: true};
-      }
-    static onlyLetters(formControl: FormControl): ValidationErrors {
-        return /[a-zA-Z]/.test(formControl.value) ? null : { onlyLetters: true};
-      }
+  static onlyDigits(formControl: FormControl): ValidationErrors {
+    return /[0-9]/.test(formControl.value) ? null : { onlyDigits: true };
+  }
+  static onlyLetters(formControl: FormControl): ValidationErrors {
+    return /[a-zA-Z]/.test(formControl.value) ? null : { onlyLetters: true };
+  }
 }
